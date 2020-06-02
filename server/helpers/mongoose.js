1b92b6548d49744039476d1ec7e605bdc9b308f3
Gagansra75 <56980688+Gagansra75@users.noreply.github.com>
2020-06-02 15:31:52 -0700

    Update mongoose.js

--- server/helpers/mongoose.js
+++ server/helpers/mongoose.js
@@ -1,7 +1,7 @@
 module.exports = {
   normalizeErrors: function(errors) {
     let normalizeErrors = [];
-
+//connect your database//
     for (let property in errors) {
       if (errors.hasOwnProperty(property)) {
         normalizeErrors.push({title: property, detail: errors[property].message});
